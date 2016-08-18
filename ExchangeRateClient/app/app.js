33ab4a21fb8438ebf6ab10aa0c1c5c7743fcfe12
Petr Šimáněk <petr.simanek@improvisio.cz>
2016-08-18 18:41:37 +0200

    delete unnecessary setting

--- ExchangeRateClient/app/app.js
+++ ExchangeRateClient/app/app.js
@@ -2,11 +2,6 @@ import angular from 'angular';
 import startRatePolling from './startRatePolling';
 
 angular.module('mews', [])
-    .config(['$httpProvider', function($httpProvider) {
-            $httpProvider.defaults.useXDomain = true;
-            delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        }
-    ])
     .controller('AppController', startRatePolling)
     .run(function() {
         console.log('App is running.');
